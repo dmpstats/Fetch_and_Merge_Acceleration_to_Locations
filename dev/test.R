d1dd8af7a643b9ee83a9460c92ad3ac4b925d8c1
Bruno Caneco <bruno@dmpstats.com>
2023-09-20 18:48:20 +0100

    deleting unused files

--- dev/test.R
+++
@@ -1,6 +0,0 @@
-library(dplyr); library(magrittr); library(stringr); library(furrr); library(sf); library(move2); library(lubridate)
-
-acc <- readRDS("data/raw/acc.rds") 
-class(acc)
-
-testACC <- accMasseur_eobsVult_updated(acc)
