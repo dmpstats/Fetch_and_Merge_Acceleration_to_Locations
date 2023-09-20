956a3bff6f2e6028e73cafd7081787fcfb88fccb
Bruno Caneco <bruno@dmpstats.com>
2023-09-20 19:11:25 +0100

    tidy up
    remove template code  and arguments/settings in `rFunction()`

--- RFunction.R
+++ RFunction.R
@@ -13,7 +13,7 @@ library("assertthat")
 # one can use the function from the logger.R file:
 # logger.fatal(), logger.error(), logger.warn(), logger.info(), logger.debug(), logger.trace()
 
-rFunction = function(data, sdk, year, usr, pwd){
+rFunction = function(data, usr, pwd){
   
   # Set movebank login credentials
   options("keyring_backend"="env")
@@ -35,30 +35,6 @@ rFunction = function(data, sdk, year, usr, pwd){
   # 5. Bind ACC data to input data 
   # TODO
   
-  logger.info(paste("Welcome to the", sdk))
-  result <- if (any(lubridate::year(mt_time(data)) == year)) { 
-    data[lubridate::year(mt_time(data)) == year,]
-  } else {
-    NULL
-  }
-  
-  
-  # if (!is.null(result)) {
-  #   # Showcase creating an app artifact. 
-  #   # This artifact can be downloaded by the workflow user on Moveapps.
-  #   artifact <- appArtifactPath("plot.png")
-  #   logger.info(paste("plotting to artifact:", artifact))
-  #   png(artifact)
-  #   plot(result)
-  #   dev.off()
-  # } else {
-  #   logger.warn("nothing to plot")
-  # }
-  # # Showcase to access a file ('auxiliary files') that is 
-  # # a) provided by the app-developer and 
-  # # b) can be overridden by the workflow user.
-  # fileName <- paste0(getAppFilePath("yourLocalFileSettingId"), "sample.txt")
-  # logger.info(readChar(fileName, file.info(fileName)$size))
 
   # provide my result to the next app in the MoveApps workflow
   return(result)
